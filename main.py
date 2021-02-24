a5dcdf1d0112bcb6ba9a9a9084383b458eb39042
hs242424 <79582976+hs242424@users.noreply.github.com>
2021-02-24 10:01:00 -0500

    Basic autoclicker

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-
+import keyboard as k
+import pyautogui as p
+toggle = False
+def setToggle():
+    global toggle
+    if toggle:
+        toggle = False
+    elif not toggle:
+        toggle = True
+while True:
+    if k.is_pressed("s"):
+        break
+    if toggle:
+        p.click()
+    if k.is_pressed("t"):
+        setToggle()
+        toggle = True
