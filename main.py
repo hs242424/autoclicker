b3d642f018e2040759d84ea0489955e8cdd4f908
hs242424 <79582976+hs242424@users.noreply.github.com>
2021-03-29 09:28:32 -0400

    Update main.py

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ def guiSetUp():
     clickVal = StringVar()
     root.title("Autoclicker")
     root.geometry('330x150')
-    label = Label(root, text="PPPPPress the button to toggle the Autoclicker").place(x=9,y=0)
+    label = Label(root, text="Press the button to toggle the Autoclicker").place(x=9,y=0)
     label3 = Label(root, text="Press the off button to turn off the Autoclicker").place(x=9,y=50)
     label2 = Label(root, text="The autoclicker is off").place(x = 9, y = 23)
     button = Button(root, text='On/Off', command = setToggle).place(x = 130, y = 23)
@@ -43,22 +43,28 @@ def guiSetUp():
     typewritebox = Entry(root,textvariable = clickVal).place(x=9,y=110)
 guiSetUp()
 while True:
-    root.update()
     if (toggle):
         root.title("Autoclicker (On)")
+        root.update()
     else:
         root.title("Autoclicker (Off)")
+        root.update()
     if(toggle):
         label2 = Label(root, text="The autoclicker is on").place(x = 9, y = 23)
+        root.update()
     else:
         label2 = Label(root, text="The autoclicker is off").place(x = 9, y = 23)
+        root.update()
     if clickVar == "click":
         label5 = Label(root, text = 'clicking').place(x = 150, y = 110)
+        root.update()
     else:
         label5 = Label(root, text = clickVar).place(x = 150, y = 110)
+        root.update()
     if k.is_pressed("enter"):              
         clickVar = clickVal.get()
         print(clickVar)
+        root.update()
     clicker()
     if (quitvar1 == 1):
         root.destroy()
